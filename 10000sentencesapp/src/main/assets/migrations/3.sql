e45449cd94cbca396178c7391e34886b228c26e1
Tomo Krajina <tkrajina@gmail.com>
2016-12-11 08:42:51 +0100

    New (stats) columns in sentence_history

---
+++ 10000sentencesapp/src/main/assets/migrations/3.sql
@@ -0,0 +1,6 @@
+alter table sentence_history add column previous_status integer;
+alter table sentence_history add column time integer;
+alter table sentence_history add column todo_count integer;
+alter table sentence_history add column repeat_count integer;
+alter table sentence_history add column done_count integer;
+alter table sentence_history add column ignore_count integer;
--- 10000sentencesapp/src/main/java/info/puzz/a10000sentences/models/SentenceHistory.java
+++ 10000sentencesapp/src/main/java/info/puzz/a10000sentences/models/SentenceHistory.java
@@ -17,8 +17,30 @@ public class SentenceHistory extends Model {
     @Column(name = "sentence_id", index = true)
     public String sentenceId;
 
+    /** New status */
     @Column(name = "status")
     public int status;
+
+    @Column(name = "previous_status")
+    public int previousStatus;
+
+    /**
+     * Time spend on this quiz
+     */
+    @Column(name = "time")
+    public int time;
+
+    @Column(name = "todo_count")
+    public int todoCount;
+
+    @Column(name = "repeat_count")
+    public int repeatCount;
+
+    @Column(name = "done_count")
+    public int doneCount;
+
+    @Column(name = "ignore_count")
+    public int ignoreCount;
 
     @Column(name = "created", index = true)
     public long created;
