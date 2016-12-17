554d79a3d39106a8e698e2691d77f09748a2a089
Tomo Krajina <tkrajina@gmail.com>
2016-12-17 09:38:24 +0100

    Added Google Analytics

--- 10000sentencesapp/src/main/java/info/puzz/a10000sentences/Application.java
+++ 10000sentencesapp/src/main/java/info/puzz/a10000sentences/Application.java
@@ -1,6 +1,8 @@
 package info.puzz.a10000sentences;
 
 import com.activeandroid.ActiveAndroid;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
@@ -8,11 +10,18 @@ public class Application extends android.app.Application {
 
     public static DiComponent COMPONENT;
 
+    public static Tracker GA_TRACKER;
+
     @Override
     public void onCreate() {
         super.onCreate();
         initIconify();
         initActiveAndroid();
+        initDagger();
+        initTracker();
+    }
+
+    private void initDagger() {
         COMPONENT = DaggerDiComponent.builder()
                 .appModule(new AppModule())
                 .build();
@@ -27,4 +36,8 @@ public class Application extends android.app.Application {
         ActiveAndroid.initialize(this);
     }
 
+    synchronized public void initTracker() {
+        GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+        GA_TRACKER = analytics.newTracker(R.xml.global_tracker);
+    }
 }
