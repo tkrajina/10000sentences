5a95750862c2b2b00671e5e5040d8ef7179bc20c
Tomo Krajina <tkrajina@gmail.com>
2017-03-22 07:13:11 +0100

    No need for done_count on language (at the moment)

--- 10000sentencesapp/src/main/assets/migrations/10.sql
+++ 10000sentencesapp/src/main/assets/migrations/10.sql
@@ -1,6 +1,4 @@
 alter table sentence_collection add column type integer;
 alter table sentence_collection add column title text;
-alter table language add column done_count integer;
 
 update sentence_collection set custom=0;
-update language set done_count=0;
